546a927d6a2fa52fb1e32625983dc0c81a92fff8
Rachel <rachel@bluelabs.com>
2014-10-21 16:02:05 -0400

    Initial github page creation
    moving files from local to being hosted at http://rg003.github.io

---
+++ app.js
@@ -0,0 +1,20 @@
+function clickBoxes(){
+	$('#work').click(function(){
+		$('#work-text').toggleClass('hidden');
+	});
+	$('#education').click(function(){
+		$('#education-text').toggleClass('hidden');
+	});
+	$('#vol').click(function(){
+		$('#vol-text').toggleClass('hidden');
+	});
+	$('#training').click(function(){
+		$('#training-text').toggleClass('hidden');
+	});
+	$('#training').click(function(){
+		$('#training-text').toggleClass('hidden');
+	});
+}
+
+clickBoxes();
+
